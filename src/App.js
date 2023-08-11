fda179ef70d749f38347d7809e8ebe823b7ec2af
naseefmw <60293453+naseefmw@users.noreply.github.com>
2023-08-11 07:26:47 +0530

    added all cse courselist

--- src/App.js
+++ src/App.js
@@ -4,18 +4,58 @@ import Chart from './components/Chart'
 import './App.css'
 
 const App = () => {
-  const [sgpa1, setSgpa1] = useState(0.0)
+  //SGPA
+  const [first, setFirst] = useState(0.0)
+  const [second, setSecond] = useState(0.0)
+  const [third, setThird] = useState(0.0)
+  const [fourth, setFourth] = useState(0.0)
+  const [fifth, setFifth] = useState(0.0)
+  const [sixth, setSixth] = useState(0.0)
+  const [seventh, setSeventh] = useState(0.0)
+  const [eighth, setEighth] = useState(0.0)
 
   const data = require('./data.json')
+  const course = data.CSE
   const courseData = [
-    [data.CSE.sem1.map((c) => c.name), data.CSE.sem2.map((c) => c.name)],
-    [data.CSE.sem1.map((c) => c.credit), data.CSE.sem2.map((c) => c.credit)],
+    [
+      course.sem1.map((c) => c.name),
+      course.sem2.map((c) => c.name),
+      course.sem3.map((c) => c.name),
+      course.sem4.map((c) => c.name),
+      course.sem5.map((c) => c.name),
+      course.sem6.map((c) => c.name),
+      course.sem7.map((c) => c.name),
+      course.sem8.map((c) => c.name),
+    ],
+    [
+      course.sem1.map((c) => c.credit),
+      course.sem2.map((c) => c.credit),
+      course.sem3.map((c) => c.credit),
+      course.sem4.map((c) => c.credit),
+      course.sem5.map((c) => c.credit),
+      course.sem6.map((c) => c.credit),
+      course.sem7.map((c) => c.credit),
+      course.sem8.map((c) => c.credit),
+    ],
   ]
 
   return (
     <div className="center">
-      <GradeCard courseData={courseData} setSgpa1={setSgpa1} />
-      sgpa1={sgpa1}
+      <GradeCard
+        courseData={courseData}
+        setGpa={[
+          setFirst,
+          setSecond,
+          setThird,
+          setFourth,
+          setFifth,
+          setSixth,
+          setSeventh,
+          setEighth,
+        ]}
+      />
+      first={first},sec={second},third={third},fourth={fourth},fifth={fifth}
+      ,sixth={sixth},seventh={seventh},eight={eighth}
       <Chart />
     </div>
   )
--- src/components/GradeCard.js
+++ src/components/GradeCard.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useState } from 'react'
 import AccordionRoot from './AccordionRoot'
 
 const listOfGrades = ['S', 'A+', 'A', 'B+', 'B', 'C+', 'C', 'D', 'P', 'F']
 const label = 'Grades'
-const titles = ['First Semester', 'Second Semester']
+const titles = [
+  'First Semester',
+  'Second Semester',
+  'Third Semester',
+  'Fourth Semester',
+  'Fifth Semester',
+  'Sixth Semester',
+  'Seventh Semester',
+  'Eighth Semester',
+]
+const totalCredits = [17, 21, 22, 22, 23, 23, 15, 17]
 
 const gpaScore = (grade) => {
   let score = 0
@@ -43,51 +53,127 @@ const gpaScore = (grade) => {
   }
   return score
 }
+const gpaCalc = (grades, credits, totalCredits, setGpa) => {
+  let score = 0
+  grades.forEach((grade, i) => {
+    score += gpaScore(grade) * credits[i]
+  })
+  let gpa = (score / totalCredits).toFixed(2)
+  console.log(gpa)
+  setGpa(gpa)
+}
 
-const GradeCard = ({ courseData, setSgpa1 }) => {
+const GradeCard = ({ courseData, setGpa }) => {
   const courseList = courseData[0]
   const credits = courseData[1]
-  const [semOneGrades, setSemOneGrades] = React.useState(
-    Array(7).fill('Select Grade')
-  )
-  const [semTwoGrades, setSemTwoGrades] = React.useState(
-    Array(2).fill('Select Grade')
-  )
 
-  const gpaCalc = (grades, credits, totalCredits) => {
-    let score = 0
-    grades.forEach((grade, i) => {
-      score += gpaScore(grade) * credits[i]
-    })
-    let gpa = (score / totalCredits).toFixed(2)
-    console.log(gpa)
-    setSgpa1(gpa)
-  }
+  const [sem1Grades, setSem1Grades] = useState(Array(7).fill('Select Grade'))
+  const [sem2Grades, setSem2Grades] = useState(Array(8).fill('Select Grade'))
+  const [sem3Grades, setSem3Grades] = useState(Array(8).fill('Select Grade'))
+  const [sem4Grades, setSem4Grades] = useState(Array(8).fill('Select Grade'))
+  const [sem5Grades, setSem5Grades] = useState(Array(8).fill('Select Grade'))
+  const [sem6Grades, setSem6Grades] = useState(Array(8).fill('Select Grade'))
+  const [sem7Grades, setSem7Grades] = useState(Array(7).fill('Select Grade'))
+  const [sem8Grades, setSem8Grades] = useState(Array(6).fill('Select Grade'))
 
   const handleSem1Change = (prop) => {
-    const updatedGrades = semOneGrades.map((grade, i) => {
+    const updatedGrades = sem1Grades.map((grade, i) => {
       if (i === parseInt(prop[0])) {
         return prop.slice(1)
       } else return grade
     })
-    setSemOneGrades(updatedGrades)
-    gpaCalc(updatedGrades, credits[0], 17)
+    setSem1Grades(updatedGrades)
+    gpaCalc(updatedGrades, credits[0], totalCredits[0], setGpa[0])
   }
+
   const handleSem2Change = (prop) => {
-    const updatedGrades = semTwoGrades.map((grade, i) => {
+    const updatedGrades = sem2Grades.map((grade, i) => {
       if (i === parseInt(prop[0])) {
         return prop.slice(1)
       } else return grade
     })
-    setSemTwoGrades(updatedGrades)
+    setSem2Grades(updatedGrades)
+    gpaCalc(updatedGrades, credits[1], totalCredits[1], setGpa[1])
+  }
+  const handleSem3Change = (prop) => {
+    const updatedGrades = sem3Grades.map((grade, i) => {
+      if (i === parseInt(prop[0])) {
+        return prop.slice(1)
+      } else return grade
+    })
+    setSem3Grades(updatedGrades)
+    gpaCalc(updatedGrades, credits[2], totalCredits[2], setGpa[2])
+  }
+  const handleSem4Change = (prop) => {
+    const updatedGrades = sem4Grades.map((grade, i) => {
+      if (i === parseInt(prop[0])) {
+        return prop.slice(1)
+      } else return grade
+    })
+    setSem4Grades(updatedGrades)
+    gpaCalc(updatedGrades, credits[3], totalCredits[3], setGpa[3])
+  }
+  const handleSem5Change = (prop) => {
+    const updatedGrades = sem5Grades.map((grade, i) => {
+      if (i === parseInt(prop[0])) {
+        return prop.slice(1)
+      } else return grade
+    })
+    setSem5Grades(updatedGrades)
+    gpaCalc(updatedGrades, credits[4], totalCredits[4], setGpa[4])
+  }
+  const handleSem6Change = (prop) => {
+    const updatedGrades = sem6Grades.map((grade, i) => {
+      if (i === parseInt(prop[0])) {
+        return prop.slice(1)
+      } else return grade
+    })
+    setSem6Grades(updatedGrades)
+    gpaCalc(updatedGrades, credits[5], totalCredits[5], setGpa[5])
+  }
+  const handleSem7Change = (prop) => {
+    const updatedGrades = sem7Grades.map((grade, i) => {
+      if (i === parseInt(prop[0])) {
+        return prop.slice(1)
+      } else return grade
+    })
+    setSem7Grades(updatedGrades)
+    gpaCalc(updatedGrades, credits[6], totalCredits[6], setGpa[6])
+  }
+  const handleSem8Change = (prop) => {
+    const updatedGrades = sem8Grades.map((grade, i) => {
+      if (i === parseInt(prop[0])) {
+        return prop.slice(1)
+      } else return grade
+    })
+    setSem8Grades(updatedGrades)
+    gpaCalc(updatedGrades, credits[7], totalCredits[7], setGpa[7])
   }
 
   return (
     <AccordionRoot
       contents={courseList}
-      active={[semOneGrades, semTwoGrades]}
+      active={[
+        sem1Grades,
+        sem2Grades,
+        sem3Grades,
+        sem4Grades,
+        sem5Grades,
+        sem6Grades,
+        sem7Grades,
+        sem8Grades,
+      ]}
       list={listOfGrades}
-      onChange={[handleSem1Change, handleSem2Change]}
+      onChange={[
+        handleSem1Change,
+        handleSem2Change,
+        handleSem3Change,
+        handleSem4Change,
+        handleSem5Change,
+        handleSem6Change,
+        handleSem7Change,
+        handleSem8Change,
+      ]}
       titles={titles}
       label={label}
     />
