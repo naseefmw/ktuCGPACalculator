9d5255da5908d70aed8c8c6fbc8364f440192c56
naseefmw <60293453+naseefmw@users.noreply.github.com>
2023-08-22 08:28:25 +0530

    chart color updated

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -4,14 +4,14 @@ import { Chart } from 'react-google-charts'
 const MyChart = ({ values }) => {
   const data = [
     ['Element', 'SGPA', { role: 'style' }],
-    ['S1', values[0][0], 'orange'],
-    ['S2', values[0][1], 'silver'],
-    ['S3', values[0][2], 'gold'],
-    ['S4', values[0][3], 'black'],
-    ['S5', values[0][4], 'black'],
-    ['S6', values[0][5], 'black'],
-    ['S7', values[0][6], 'black'],
-    ['S8', values[0][7], 'black'],
+    ['S1', values[0][0], 'blue'],
+    ['S2', values[0][1], 'blue'],
+    ['S3', values[0][2], 'blue'],
+    ['S4', values[0][3], 'blue'],
+    ['S5', values[0][4], 'blue'],
+    ['S6', values[0][5], 'blue'],
+    ['S7', values[0][6], 'blue'],
+    ['S8', values[0][7], 'blue'],
     ['CGPA', values[1], 'black'],
   ]
   return (
